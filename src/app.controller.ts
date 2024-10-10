d21a9b05256752b9632d16151e65aca1af78688f
cosmodesant49 <emilmaldybaev49@gmail.com>
2024-10-10 20:37:03 +0600

    байель почини

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { RateLimit } from 'nestjs-rate-limiter';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  // Ограничение на 2 запроса за 30 секунд для этого маршрута
+  @RateLimit({ points: 2, duration: 30 })
   @Get()
   getHello(): string {
     return this.appService.getHello();
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,30 @@
-import { Module } from '@nestjs/common';
+import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
+import { RateLimiterModule } from 'nestjs-rate-limiter';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TaskModule } from './task/task.module';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+import { RateLimiterMiddleware } from './rate-limiter.middleware'; // Импортируйте middleware
 
 @Module({
-  imports: [TaskModule, AuthModule, UsersModule],
+  imports: [
+    RateLimiterModule.register({
+      points: 100, // Глобальное ограничение в 100 запросов за час
+      duration: 3600, // 1 час
+      keyPrefix: 'global', // Префикс ключа
+    }),
+    TaskModule,
+    AuthModule,
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(RateLimiterMiddleware) // Подключение вашего middleware
+      .forRoutes('*'); // Применить ко всем маршрутам
+  }
+}
